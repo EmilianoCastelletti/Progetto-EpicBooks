94f9d373f2ad4e85cc2fee6a90635bd81e52404b
EmilianoCastelletti <168585555+EmilianoCastelletti@users.noreply.github.com>
2024-10-07 11:59:34 +0200

    Add files via upload

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,39 @@
+import { Button, ListGroup } from 'react-bootstrap'
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzAzOWFiNDY5OGYxNzAwMTU2MDQ0MTciLCJpYXQiOjE3MjgyODk0NjAsImV4cCI6MTcyOTQ5OTA2MH0.jzvR4rAbs20PcnTNuMjpWlz4JcfQbajxM6irlFridpQ',
+          },
+        }
+      )
+      if (response.ok) {
+        alert('La recensione è stata elimata!')
+      } else {
+        throw new Error('La recensione non è stata eliminata!')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  return (
+    <ListGroup.Item data-testid="single-comment">
+      {comment.comment}
+      <Button
+        variant="danger"
+        className="ms-2"
+        onClick={() => deleteComment(comment._id)}
+      >
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  )
+}
+
+export default SingleComment
